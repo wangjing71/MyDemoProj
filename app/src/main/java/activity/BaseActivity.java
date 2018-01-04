6ad876326be402d43ed9e63b8e2723b406fd8740
WangJing <397383523@qq.com>
2018-01-04 23:12:13 +0800

    Initial commit

---
+++ app/src/main/java/activity/BaseActivity.java
@@ -0,0 +1,63 @@
+package activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gyf.barlibrary.ImmersionBar;
+
+/**
+ * Created by Administrator on 2018/1/4.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private ImmersionBar mImmersionBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(setLayoutId());
+
+        if (isImmersionBarEnabled()){
+            initImmersionBar();
+        }
+        initView();
+        initData();
+        setEvent();
+    }
+
+    protected abstract int setLayoutId();
+
+    protected void initView() {
+    }
+
+    protected void initData() {
+    }
+
+    protected void setEvent() {
+    }
+
+    /**
+     * 是否可以使用沉浸式
+     * Is immersion bar enabled boolean.
+     *
+     * @return the boolean
+     */
+    protected boolean isImmersionBarEnabled() {
+        return true;
+    }
+
+    protected void initImmersionBar() {
+        //在BaseActivity里初始化
+        mImmersionBar = ImmersionBar.with(this);
+        mImmersionBar.init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mImmersionBar != null)
+            mImmersionBar.destroy();  //在BaseActivity里销毁
+    }
+}
