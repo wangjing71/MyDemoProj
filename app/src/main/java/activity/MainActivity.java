a13bb6b0ac174a84c78e0a9c0c498de79abf576a
WangJing <397383523@qq.com>
2018-01-06 21:56:02 +0800

    1

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,15 +1,27 @@
 package activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.wj.myproj.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int setLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+    }
+
+    @Override
+    protected void initData() {
+    }
+
+    @Override
+    protected void setEvent() {
     }
 }
