256ce497603681857073ae3c9c548888d5a2bf3e
WangJing <397383523@qq.com>
2018-01-07 01:06:08 +0800

    222

--- app/src/main/java/com/wj/fragment/HomeThreeFragment.java
+++ app/src/main/java/com/wj/fragment/HomeThreeFragment.java
@@ -1,21 +1,26 @@
 package com.wj.fragment;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.ColorUtils;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.gyf.barlibrary.ImmersionBar;
+import com.lcodecore.tkrefreshlayout.RefreshListenerAdapter;
 import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
-import com.wj.adapter.MainListViewApapter;
+import com.wj.adapter.OneAdapter;
 import com.wj.myproj.R;
 import com.wj.utils.GlideImageLoader;
 import com.youth.banner.Banner;
@@ -30,14 +35,14 @@ import java.util.List;
 public class HomeThreeFragment extends Fragment {
 
     private Toolbar mToolbar;
-    private ListView mListView;
+    private RecyclerView mRv;
     private TwinklingRefreshLayout refreshLayout;
     private View view;
     private List<String> mImages = new ArrayList<>();
     private List<String> mItemList = new ArrayList<>();
-    private MainListViewApapter adapter;
     private View headView;
     private int bannerHeight;
+    private OneAdapter mOneAdapter;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
@@ -53,7 +58,7 @@ public class HomeThreeFragment extends Fragment {
 
     private void initView() {
         mToolbar = view.findViewById(R.id.toolbar);
-        mListView = view.findViewById(R.id.mlistview);
+        mRv = view.findViewById(R.id.rv);
         refreshLayout = view.findViewById(R.id.refreshLayout);
 
     }
@@ -68,26 +73,113 @@ public class HomeThreeFragment extends Fragment {
         mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
         mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
 
-        adapter = new MainListViewApapter(getContext());
-        adapter.setmItemList(mItemList);
-        mListView.setAdapter(adapter);
 
+        refreshLayout.setEnableLoadmore(false);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.VERTICAL, false);
+        mRv.setLayoutManager(linearLayoutManager);
+        mOneAdapter = new OneAdapter();
+        mOneAdapter.openLoadAnimation(BaseQuickAdapter.SCALEIN);
+        mRv.setAdapter(mOneAdapter);
+        addHeaderView();
+        mOneAdapter.setPreLoadNumber(1);
+        mOneAdapter.setNewData(mItemList);
+
+
+    }
+
+    private void setEvents() {
+        mRv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            private int totalDy = 0;
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                totalDy += dy;
+                if (totalDy <= bannerHeight) {
+                    float alpha = (float) totalDy / bannerHeight;
+                    mToolbar.setBackgroundColor(ColorUtils.blendARGB(Color.TRANSPARENT
+                            , ContextCompat.getColor(getActivity(), R.color.colorPrimary), alpha));
+                } else {
+                    mToolbar.setBackgroundColor(ColorUtils.blendARGB(Color.TRANSPARENT
+                            , ContextCompat.getColor(getActivity() , R.color.colorPrimary), 1));
+                }
+            }
+        });
+        mOneAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+            @Override
+            public void onLoadMoreRequested() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mOneAdapter.addData(addData());
+                        if (mItemList.size() == 100) {
+                            mOneAdapter.loadMoreEnd();
+                        } else
+                            mOneAdapter.loadMoreComplete();
+                    }
+                }, 2000);
+            }
+        }, mRv);
+        refreshLayout.setOnRefreshListener(new RefreshListenerAdapter() {
+            @Override
+            public void onRefresh(final TwinklingRefreshLayout refreshLayout) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mItemList.clear();
+                        mItemList.addAll(newData());
+                        mOneAdapter.setNewData(mItemList);
+                        refreshLayout.finishRefreshing();
+                        mToolbar.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+            }
+
+            @Override
+            public void onPullingDown(TwinklingRefreshLayout refreshLayout, float fraction) {
+                mToolbar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onPullDownReleasing(TwinklingRefreshLayout refreshLayout, float fraction) {
+                if (Math.abs(fraction - 1.0f) > 0) {
+                    mToolbar.setVisibility(View.VISIBLE);
+                } else {
+                    mToolbar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    private void addHeaderView() {
         if (mImages != null && mImages.size() > 0) {
-            headView = LayoutInflater.from(getActivity()).inflate(R.layout.item_banner, (ViewGroup) mListView.getParent(), false);
+            headView = LayoutInflater.from(getActivity()).inflate(R.layout.item_banner, (ViewGroup) mRv.getParent(), false);
             Banner banner = (Banner) headView.findViewById(R.id.banner);
             banner.setImages(mImages)
                     .setImageLoader(new GlideImageLoader())
                     .setDelayTime(5000)
                     .start();
-            mListView.addHeaderView(headView);
+            mOneAdapter.addHeaderView(headView);
             ViewGroup.LayoutParams bannerParams = banner.getLayoutParams();
             ViewGroup.LayoutParams titleBarParams = mToolbar.getLayoutParams();
             bannerHeight = bannerParams.height - titleBarParams.height - ImmersionBar.getStatusBarHeight(getActivity());
         }
-
     }
 
-    private void setEvents() {
+    private List<String> addData() {
+        List<String> data = new ArrayList<>();
+        for (int i = mItemList.size() + 1; i <= mItemList.size() + 20; i++) {
+            data.add("item" + i);
+        }
+        return data;
+    }
 
+    private List<String> newData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            data.add("item" + i);
+        }
+        return data;
     }
 }
