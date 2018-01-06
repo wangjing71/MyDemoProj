5c18e5b7c8c33fe15cd3de8dc3f8cfa48e302198
WangJing <397383523@qq.com>
2018-01-06 23:48:56 +0800

    222

--- app/src/main/java/com/wj/fragment/HomeThreeFragment.java
+++ app/src/main/java/com/wj/fragment/HomeThreeFragment.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.gyf.barlibrary.ImmersionBar;
 import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
+import com.wj.adapter.MainListViewApapter;
 import com.wj.myproj.R;
+import com.wj.utils.GlideImageLoader;
+import com.youth.banner.Banner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,13 +30,14 @@ import java.util.List;
 public class HomeThreeFragment extends Fragment {
 
     private Toolbar mToolbar;
-    private RecyclerView mRecyclerView;
+    private ListView mListView;
     private TwinklingRefreshLayout refreshLayout;
-    private int bannerHeight;
-    private View headView;
     private View view;
     private List<String> mImages = new ArrayList<>();
     private List<String> mItemList = new ArrayList<>();
+    private MainListViewApapter adapter;
+    private View headView;
+    private int bannerHeight;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
@@ -47,7 +53,7 @@ public class HomeThreeFragment extends Fragment {
 
     private void initView() {
         mToolbar = view.findViewById(R.id.toolbar);
-        mRecyclerView = view.findViewById(R.id.rv);
+        mListView = view.findViewById(R.id.mlistview);
         refreshLayout = view.findViewById(R.id.refreshLayout);
 
     }
@@ -56,10 +62,29 @@ public class HomeThreeFragment extends Fragment {
         for (int i = 1; i <= 20; i++) {
             mItemList.add("item" + i);
         }
+
         mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
         mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
         mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
         mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
+
+        adapter = new MainListViewApapter(getContext());
+        adapter.setmItemList(mItemList);
+        mListView.setAdapter(adapter);
+
+        if (mImages != null && mImages.size() > 0) {
+            headView = LayoutInflater.from(getActivity()).inflate(R.layout.item_banner, (ViewGroup) mListView.getParent(), false);
+            Banner banner = (Banner) headView.findViewById(R.id.banner);
+            banner.setImages(mImages)
+                    .setImageLoader(new GlideImageLoader())
+                    .setDelayTime(5000)
+                    .start();
+            mListView.addHeaderView(headView);
+            ViewGroup.LayoutParams bannerParams = banner.getLayoutParams();
+            ViewGroup.LayoutParams titleBarParams = mToolbar.getLayoutParams();
+            bannerHeight = bannerParams.height - titleBarParams.height - ImmersionBar.getStatusBarHeight(getActivity());
+        }
+
     }
 
     private void setEvents() {
