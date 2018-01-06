344fe04bf7dff16a90300eb2ba32b43f2722e1be
WangJing <397383523@qq.com>
2018-01-06 23:12:02 +0800

    222

--- app/src/main/java/com/wj/fragment/HomeThreeFragment.java
+++ app/src/main/java/com/wj/fragment/HomeThreeFragment.java
@@ -6,14 +6,17 @@ import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toolbar;
 
 import com.gyf.barlibrary.ImmersionBar;
 import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
 import com.wj.myproj.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018/1/6.
@@ -27,6 +30,8 @@ public class HomeThreeFragment extends Fragment {
     private int bannerHeight;
     private View headView;
     private View view;
+    private List<String> mImages = new ArrayList<>();
+    private List<String> mItemList = new ArrayList<>();
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Nullable
@@ -48,7 +53,13 @@ public class HomeThreeFragment extends Fragment {
     }
 
     private void initData() {
-
+        for (int i = 1; i <= 20; i++) {
+            mItemList.add("item" + i);
+        }
+        mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
+        mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
+        mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
+        mImages.add("http://desk.zol.com.cn/showpic/1024x768_63850_14.html");
     }
 
     private void setEvents() {
